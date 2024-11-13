df356d9de7e31e0ecd7dd6d3eca837ad5299fd4c
jeroalvarez1 <jeroalvarez1@gmail.com>
2024-11-13 02:38:12 -0300

    add version feature

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL") # "postgresql://openpg:openpgpwd@localhost:5442/x_men_mutants"
+DATABASE_URL = "postgresql://openpg:openpgpwd@localhost:5442/x_men_mutants"#os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.versions.v2.routers import human as v2_human
 from app.versions.v3.routers import human as v3_human
 from app.database import engine, Base
-from app.versions.v3.models.human import Human
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,5 +18,12 @@ app = FastAPI()
 async def main():
     return RedirectResponse(url="/docs")
 
-app.include_router(v2_human.router, prefix="/api/v2/human", tags=["Human V2"])
-app.include_router(v3_human.router, prefix="/api/v3/human", tags=["Human V3"])
+api_version = os.getenv('API_VERSION')
+
+if api_version == 'v2':
+    app.include_router(v2_human.router, prefix="/api/v2/human", tags=["Human V2"])
+elif api_version == 'v3':
+    app.include_router(v3_human.router, prefix="/api/v3/human", tags=["Human V3"])
+else:
+    app.include_router(v2_human.router, prefix="/api/v2/human", tags=["Human V2"])
+    app.include_router(v3_human.router, prefix="/api/v3/human", tags=["Human V3"])
