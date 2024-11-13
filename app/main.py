4c665389533637028e6af17308246b757c460e82
jeroalvarez1 <jeroalvarez1@gmail.com>
2024-11-13 02:53:52 -0300

    test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.versions.v2.routers import human as v2_human
 from app.versions.v3.routers import human as v3_human
 from app.database import engine, Base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 Base.metadata.create_all(bind=engine)
@@ -14,6 +18,12 @@ app = FastAPI()
 async def main():
     return RedirectResponse(url="/docs")
 
+api_version = os.getenv('API_VERSION')
 
-app.include_router(v2_human.router, prefix="/api/v2/human", tags=["Human V2"])
-app.include_router(v3_human.router, prefix="/api/v3/human", tags=["Human V3"])
+if api_version == 'v2':
+    app.include_router(v2_human.router, prefix="/api/v2/human", tags=["Human V2"])
+elif api_version == 'v3':
+    app.include_router(v3_human.router, prefix="/api/v3/human", tags=["Human V3"])
+else:
+    app.include_router(v2_human.router, prefix="/api/v2/human", tags=["Human V2"])
+    app.include_router(v3_human.router, prefix="/api/v3/human", tags=["Human V3"])
